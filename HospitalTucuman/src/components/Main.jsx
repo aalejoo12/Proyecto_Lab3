9ab924eec65a99c90d721f900634932bd745bdf9
Alejo <aalejoo12@gmail.com>
2024-06-29 20:28:06 -0300

    intento de actualizar

--- HospitalTucuman/src/components/Main.jsx
+++ HospitalTucuman/src/components/Main.jsx
@@ -97,10 +97,12 @@ const Main = () => {
     }
   };
 
-  const handleActualizar = async (e, id_paciente) => {
-    // e.preventDefault()
+  const handleActualizar = async (e) => {
+      
+    e.preventDefault()
+
     try {
-      const response = await axios.put(`http://localhost:8000/pacientes/editar/${id_paciente}`, {
+      const response = await axios.put(`http://localhost:8000/pacientes/editar/${idActualizar}`, {
         nombre: nomyape,
         edad: edad,
         email: email,
@@ -127,8 +129,8 @@ const Main = () => {
 
 
   const handleEditar = async (id_paciente) => {
+  
     setIdActualizar(id_paciente)
-    console.log(idActualizar);
     let result = await axios.get("http://localhost:8000/pacientes");
 
     console.log(result.data[id_paciente - 1]);
@@ -244,16 +246,14 @@ const Main = () => {
               <Button variant="success" type="submit" onClick={handleChange}>
                 Agregar
               </Button>
-
-            </div>
-            <div className="text-center mt-5">
-
-              <Button variant="success" type="button" onClick={() => handleActualizar(idActualizar)}>
+              <Button variant="primary" type="button" onClick={handleActualizar}>
                 Actualizar
               </Button>
 
-
             </div>
+
+
+
           </Row>
         </Form>
       </div>
@@ -339,7 +339,7 @@ const Main = () => {
                         />
                       </svg>
                     </button>
-                    <button className="editBtn" onClick={() => handleEditar(paciente.id_paciente)}
+                    <button className="editBtn" onClick={()=>handleEditar(paciente.id_paciente)}
                     >
                       <svg height="1em" viewBox="0 0 512 512">
                         <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
