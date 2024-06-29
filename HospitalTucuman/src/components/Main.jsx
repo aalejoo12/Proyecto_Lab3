9ab656726692b590b03a66caebf1aca06dad9eb7
Alejo <aalejoo12@gmail.com>
2024-06-29 20:10:21 -0300

    cambios de estilo

--- HospitalTucuman/src/components/Main.jsx
+++ HospitalTucuman/src/components/Main.jsx
@@ -118,7 +118,9 @@ const Main = () => {
   // console.log(opcion,internar);
 
   return (
-    <>
+    <>  <div className="text-center mt-5">
+        <h2>Agrega un paciente</h2>
+        </div>
       <div className="container-form">
         <Form className="form" onSubmit={handleSubmit}>
           <Row className="mb-3">
@@ -155,7 +157,7 @@ const Main = () => {
               value={email}
               name="email"
               type="email"
-              placeholder="example@example.com"
+              placeholder="ejemplo@ejemplo.com"
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
