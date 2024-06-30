73955da65fd71bb3e649918f61481fdb6984572c
bruno <bazanbruno199@gmail.com>
2024-06-30 11:43:11 -0300

    agregar medicos listo ??

--- HospitalTucuman/src/components/Main.jsx
+++ HospitalTucuman/src/components/Main.jsx
@@ -77,7 +77,6 @@ const Main = () => {
   const handleEliminar = async () => {
     console.log(idAeliminar);
     handleClose(true)
-    handleClose(true)
 
     let response = await axios.delete(
       `http://localhost:8000/pacientes/eliminar/${idAeliminar}`
@@ -140,7 +139,7 @@ const Main = () => {
     setMostrar(true)
     setMostrar2(false)
     setIdActualizar(id_paciente)
-    let result = await axios.get(`http://localhost:8000/pacientes/${id_paciente}`);
+    let result = await axios.get("http://localhost:8000/pacientes");
 
     console.log(result.data[0]);
     if (result) {
@@ -151,7 +150,7 @@ const Main = () => {
       setDni(result.data[0].dni)
       setTelefono(result.data[0].telefono)
     }
-    
+
   };
 
   useEffect(() => {
@@ -164,8 +163,8 @@ const Main = () => {
 
   return (
     <>  <div className="text-center mt-5">
-        <h2>Agrega un paciente</h2>
-        </div>
+      <h2>Agrega un paciente</h2>
+    </div>
       <div className="container-form">
         <Form className="form" onSubmit={handleSubmit} >
           <Row className="mb-3">
@@ -352,7 +351,7 @@ const Main = () => {
                         />
                       </svg>
                     </button>
-                    <button className="editBtn" onClick={()=>handleEditar(paciente.id_paciente)}
+                    <button className="editBtn" onClick={() => handleEditar(paciente.id_paciente)}
                     >
                       <svg height="1em" viewBox="0 0 512 512">
                         <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
--- HospitalTucuman/src/pages/Medicos.jsx
+++ HospitalTucuman/src/pages/Medicos.jsx
@@ -3,18 +3,67 @@ import "../css/Medicos.css"
 import Header from '../components/Header'
 import Sidebar from "../components/Sidebar";
 import Footer from '../components/Footer';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Form, ListGroup, Row } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Medicos = () => {
   const [medicos, setMedicos] = useState([])
+  const [mostrar, setMostrar] = useState(false);
+  const [mostrar2, setMostrar2] = useState(true);
+
+  const [nomyape, setNomyape] = useState("");
+  const [especialidad, setEspecialidad] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [imagen, setImagen] = useState();
 
   const getMedicos = async () => {
     let result = await axios.get("http://localhost:8000/medicos");
     console.log(result.data);
     setMedicos(result.data);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (nomyape && especialidad && email && telefono && imagen != "") {
+      handleChange()
+
+      const response = axios.post("http://localhost:8000/medicos/agregar", {
+        nombre: nomyape,
+        especialidad: especialidad,
+        email: email,
+        telefono: telefono,
+        imagen: imagen,
+      });
+      if (response) {
+        alert("medico creado");
+      }
+    } else {
+      alert("debe ingresar todos los campos");
+    }
+    setNomyape("");
+    setEspecialidad("");
+    setEmail("");
+    setTelefono("");
+    setImagen("");
+
+    e.target.reset();
+    getMedicos();
+  }
+  const handleChange = () => {
+
+  }
+
+
+  const handleActualizar = async() => {
+
+    setMostrar(false)
+    setMostrar2(true)
+  }
+  const handleEditar = async() => {
+    setMostrar(true)
+    setMostrar2(false)
+  }
 
 
   useEffect(() => {
@@ -27,6 +76,115 @@ const Medicos = () => {
   return (
     <>
 
+      <div className="text-center mt-5">
+        <h2>Agrega un medico</h2>
+      </div>
+      <div className="container-form">
+        <Form className="form" onSubmit={handleSubmit} >
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Nombre y Apellido</Form.Label>
+              <Form.Control
+                // value={nomyape}
+                name="nomyape"
+                type="text"
+                placeholder="Ingresa nombre y apellido"
+                onChange={(e) => {
+                  setNomyape(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridEspecialidad">
+              <Form.Label>Especialidad</Form.Label>
+              <Form.Select
+                onChange={(e) => {
+                  setEspecialidad(e.target.value);
+                }}
+                defaultValue="Chose..."
+              >
+                <option value="">Elije una especialidad</option>
+                <option value="cardiologia">Cardiología</option>
+                <option value="pediatria">Pediatría</option>
+                <option value="neurologia">Neurología</option>
+                <option value="dermatologia">Dermatología</option>
+                <option value="ginecologia">Ginecología</option>
+                <option value="oftalmologia">Oftalmología</option>
+                <option value="traumatologia">Traumatología</option>
+                <option value="psiquiatria">Psiquiatría</option>
+                <option value="urologia">Urología</option>
+                <option value="endocrinologia">Endocrinología</option>
+              </Form.Select>
+            </Form.Group>
+
+
+
+          </Row>
+
+          <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              // value={email}
+              name="email"
+              type="email"
+              placeholder="ejemplo@ejemplo.com"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </Form.Group>
+
+
+
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>Telefono</Form.Label>
+              <Form.Control
+                // value={telefono}
+                name="telefono"
+                type="number"
+                placeholder="Teléfono"
+                onChange={(e) => {
+                  setTelefono(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGridAddress2">
+              <Form.Label>Imagen</Form.Label>
+              <Form.Control
+                // value={imagen}
+                name="imagen"
+                type="text"
+                placeholder="Ingrese URL de la imagen"
+                onChange={(e) => {
+                  setImagen(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+
+          </Row>
+          <Row>
+
+
+            <div className="text-center mt-5">
+              {mostrar2 && <Button variant="primary" type="submit" onClick={handleChange}>
+                Agregar
+              </Button>}
+
+              {mostrar && <Button variant="warning" type="button" onClick={handleActualizar}>
+                Actualizar
+              </Button>}
+
+
+            </div>
+
+
+
+          </Row>
+        </Form>
+      </div>
+
       <Header />
       <div className='d-flex justify-content-center'>
         <Row md={4} >
@@ -36,11 +194,14 @@ const Medicos = () => {
                 <Card.Img variant="top" src={medico.imagen} />
                 <Card.Body>
                   <Card.Title>{medico.nombre}</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Especialidad: {medico.especialidad}</ListGroup.Item>
+                    <ListGroup.Item>Email: {medico.email}</ListGroup.Item>
+                    <ListGroup.Item>Telefono: {medico.telefono}</ListGroup.Item>
+                  </ListGroup>
+                  <Button variant="primary" onClick={handleEditar}>editar</Button>
+                  <Button variant="danger">Eliminar</Button>
+
                 </Card.Body>
               </Card>
             </Col>
