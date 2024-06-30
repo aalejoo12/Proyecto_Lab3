1c11614fe080f8be37676768fc3898da7ab1db95
bruno <bazanbruno199@gmail.com>
2024-06-30 17:41:09 -0300

    back turnos listo

--- back/controllers/turnos.js
+++ back/controllers/turnos.js
@@ -1,24 +1,20 @@
 const { conection } = require("../config/db");
 
 const todoTurnos = (req, res) => {
-    
-  const query = "select * from Turnos";
+  const query = "select p.id_paciente, p.nombre as NombrePaciente, t.fecha as Fecha, t.hora as Hora, m.nombre as Medico from Turnos t join Pacientes p on p.id_paciente = t.id_paciente join Medicos M on m.id_medico = t.id_medico";
 
   conection.query(query, (err, results) => {
     if (err) throw err;
     res.json(results);
   });
-};  
+};
 
 const agregarTurnos = (req, res) => {
   console.log(req);
 
-  const { fecha, hora, medico, paciente } = req.body;
+  const { fecha, hora, id_medico, id_paciente } = req.body;
 
-  const fechaNueva = new Date(fecha).toISOString().split('T')[0]; // "2024-06-10"
-
-
-  const query = `INSERT INTO Turnos (fecha, hora, id_medico, id_paciente) VALUES ("${fechaNueva}","${hora}",${medico},${paciente})`;
+  const query = `INSERT INTO turnos (fecha, hora, id_medico, id_paciente) VALUES ("${fecha}","${hora}",${id_medico},${id_paciente})`;
 
   conection.query(query, (err, results) => {
     if (err) throw err;
@@ -28,7 +24,7 @@ const agregarTurnos = (req, res) => {
 
 const borrarTurnos = (req, res) => {
   const id = req.params.id;
-  const query = `delete from pacientes where id_Turnos=${id}`;
+  const query = `delete from turnos where id_turno=${id}`;
   conection.query(query, (err, results) => {
     if (err) throw err;
     res.send(results);
@@ -37,9 +33,9 @@ const borrarTurnos = (req, res) => {
 
 const editarTurnos = (req, res) => {
   const id = req.params.id;
-  const { nombre, edad, dni, email, telefono } = req.body;
+  const { fecha, hora, id_medico, id_paciente } = req.body;
 
-  const query = `UPDATE Turnos SET nombre = '${nombre}', edad = '${edad}', dni = '${dni}', email = '${email}', telefono = '${telefono}' WHERE id_Turnos = ${id}`;
+  const query = `UPDATE turnos SET fecha = '${fecha}', hora = '${hora}', id_medico = ${id_medico}, id_paciente = ${id_paciente} WHERE id_turno = ${id}`;
   conection.query(query, (err, results) => {
     if (err) throw err;
     res.send(results);
@@ -48,7 +44,7 @@ const editarTurnos = (req, res) => {
 
 const verTurnos = (req, res) => {
   const id = req.params.id;
-  const query = `select * from Turnos where id_Turnos=${id}`;
+  const query = `select * from turnos where id_turno=${id}`;
   conection.query(query, (err, results) => {
     if (err) throw err;
     res.send(results);
