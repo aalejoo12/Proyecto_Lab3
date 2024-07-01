49ee123be1d5b87fbe04a442bc0ff434926ef863
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-01 05:10:32 -0300

    añado comentarios al index.js del back

--- back/index.js
+++ back/index.js
@@ -1,3 +1,4 @@
+//importa express, la conexion a la base de datos y los modulos de las rutas
 const express = require("express");
 const { conection } = require("./config/db");
 const pacientes = require("./routes/pacientes");
@@ -7,32 +8,42 @@ const turnos = require("./routes/turnos");
 const hc = require("./routes/hc");
 const estudios = require("./routes/estudios");
 
-
+//importa cors
 const cors = require("cors");
 
+//crea una variable app en la que almacena la instancia express
 const app = express();
 
+//crea una variable port que el servidor escuchara
 const port = 8000;
 
+//permite manejar solicutudes json
 app.use(express.json());
+//permite habilitar cors
 app.use(cors());
+
+//asocia cada modulo de rutas a la ruta raiz ('/')
+//Esto significa que todas las rutas definidas en esos módulos estarán disponibles desde la raíz del servidor.
 app.use("/", pacientes);
 app.use("/", logins);
 app.use("/", medicos);
 app.use("/", turnos);
-app.use ("/", hc)
-app.use ("/", estudios)
+app.use("/", hc);
+app.use("/", estudios);
 
-
+//inicia la conexion a la base de datos y muestra un mensaje cuando la conexion es exitosa
 conection.connect(() => {
   console.log("conectado a mi base de datos");
 });
 
+//Define una ruta GET en la raíz del servidor que imprime "benvenute" en la consola y responde con un mensaje JSON de bienvenida.
 app.get("/", (req, res) => {
   console.log("benvenute");
   res.send({ message: "bienvenido a mi API" });
 });
 
+//Inicia el servidor y lo hace escuchar en el puerto definido (8000).
+//Muestra un mensaje en la consola cuando el servidor está corriendo.
 app.listen(port, () => {
   console.log("escuchando " + port);
 });
