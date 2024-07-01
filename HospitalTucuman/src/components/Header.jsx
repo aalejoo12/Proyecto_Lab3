b14bf0f2a653233c0b25f5ee550c5042e158f12d
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-01 05:43:12 -0300

    añado comentarios a los componentes

--- HospitalTucuman/src/components/Header.jsx
+++ HospitalTucuman/src/components/Header.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import "../css/Header.css"
-import { Navbar,Container, NavbarText, Nav, Image } from 'react-bootstrap'
+//importa React, css, y componentes de react-bootstrap
+import React from "react";
+import "../css/Header.css";
+import { Navbar, Container, NavbarText, Nav, Image } from "react-bootstrap";
 
-
-
+//define el componente header
 const Header = () => {
-
-
+  //este componente muestra
   return (
+    //un navbar con una clase que hace que se mueva a la derecha
     <Navbar className="navbar">
-    <Container fluid className='d-flex justify-content-center'>
-      <Navbar.Brand className='justify-content-end' href="#home">
-        
-        <img
-          alt=""
-          src="../public/signo-de-hospital.ico"
-          width="45"
-          height="45"
-          className="d-inline-block align-top"
-        />{' '}
-        <NavbarText className='titulo' >Hospital Tucumán</NavbarText>
-      </Navbar.Brand>
-    </Container>
-    <div className='justify-content-end me-5' >
-    <Nav>
+      {/* un contenedor de ancho completo que usa flexbox y que esta centrado */}
+      <Container fluid className="d-flex justify-content-center">
+        <Navbar.Brand className="justify-content-end" href="#home">
+          {/* una imagen */}
+          <img
+            alt=""
+            src="../public/signo-de-hospital.ico"
+            width="45"
+            height="45"
+            className="d-inline-block align-top"
+          />{" "}
+          {/* Un titulo que tiene css que le cambia el grosor y el tamaño de la letra */}
+          <NavbarText className="titulo">Hospital Tucumán</NavbarText>
+        </Navbar.Brand>
+      </Container>
+      {/* un div que esta al extremo derecho */}
+      <div className="justify-content-end me-5">
+        <Nav>
+          {/* una imagen */}
           <Image
             src="../public/images.jpeg" // Cambia esto a la ruta de tu imagen
             roundedCircle
@@ -32,9 +36,10 @@ const Header = () => {
             className="circular-frame"
           />
         </Nav>
-        </div>
-  </Navbar>
-  )
-}
+      </div>
+    </Navbar>
+  );
+};
 
-export default Header
+//exporta el componente header
+export default Header;
--- HospitalTucuman/src/components/Main.jsx
+++ HospitalTucuman/src/components/Main.jsx
@@ -1,3 +1,4 @@
+//importa useEffect, useState de react, css, varios componentes de react-bootstrap, axios, Sidebar, Link de react-router-dom e Historial
 import { useEffect, useState } from "react";
 import "../css/Main.css";
 import {
@@ -14,10 +15,12 @@ import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
 import Historial from "../pages/Historial";
 
+//define el componente Main
 const Main = () => {
+  //crea un estado vacio de un array que ira mutando y contendra pacientes
   const [pacientes, setPacientes] = useState([]);
 
-
+  //crea todos los estados que mutaran
   const [nomyape, setNomyape] = useState("");
   const [edad, setEdad] = useState("");
   const [email, setEmail] = useState("");
@@ -30,7 +33,6 @@ const Main = () => {
   const [idActualizar, setIdActualizar] = useState(null);
   const [mostrar, setMostrar] = useState(false);
   const [mostrar2, setMostrar2] = useState(true);
-
 
   const handleClose = () => {
     setShow(false);
@@ -78,7 +80,7 @@ const Main = () => {
 
   const handleEliminar = async () => {
     console.log(idAeliminar);
-    handleClose(true)
+    handleClose(true);
 
     let response = await axios.delete(
       `http://localhost:8000/pacientes/eliminar/${idAeliminar}`
@@ -91,7 +93,6 @@ const Main = () => {
   };
 
   const handleChange = () => {
-
     // parte de agregar
 
     if (opcion === "SI") {
@@ -106,18 +107,20 @@ const Main = () => {
   const handleActualizar = async (e) => {
     getPacientes();
 
-    setMostrar(false)
-    setMostrar2(true)
-    e.preventDefault()
+    setMostrar(false);
+    setMostrar2(true);
+    e.preventDefault();
     try {
-      const response = await axios.put(`http://localhost:8000/pacientes/editar/${idActualizar}`, {
-
-        nombre: nomyape,
-        edad: edad,
-        email: email,
-        dni: dni,
-        telefono: telefono
-      });
+      const response = await axios.put(
+        `http://localhost:8000/pacientes/editar/${idActualizar}`,
+        {
+          nombre: nomyape,
+          edad: edad,
+          email: email,
+          dni: dni,
+          telefono: telefono,
+        }
+      );
       if (response.status === 200) {
         alert("Paciente actualizado correctamente");
         getPacientes(); // Opcional: actualizar la lista después de la actualización
@@ -133,28 +136,24 @@ const Main = () => {
     setInternar("");
 
     getPacientes();
-
-  }
-
+  };
 
   const handleEditar = async (id_paciente) => {
-    setMostrar(true)
-    setMostrar2(false)
-    setIdActualizar(id_paciente)
+    setMostrar(true);
+    setMostrar2(false);
+    setIdActualizar(id_paciente);
     let result = await axios.get("http://localhost:8000/pacientes");
 
-      // Buscamos el médico correspondiente al id_medico proporcionado
-      const paciente = result.data.find(m => m.id_paciente === id_paciente);
+    // Buscamos el médico correspondiente al id_medico proporcionado
+    const paciente = result.data.find((m) => m.id_paciente === id_paciente);
 
     if (paciente) {
-      
-      setNomyape(paciente.nombre)
-      setEdad(paciente.edad)
-      setEmail(paciente.email)
-      setDni(paciente.dni)
-      setTelefono(paciente.telefono)
+      setNomyape(paciente.nombre);
+      setEdad(paciente.edad);
+      setEmail(paciente.email);
+      setDni(paciente.dni);
+      setTelefono(paciente.telefono);
     }
-
   };
 
   useEffect(() => {
@@ -166,11 +165,13 @@ const Main = () => {
   // console.log(opcion,internar);
 
   return (
-    <>  <div className="text-center mt-5 container-titulo">
-      <h2>Agrega un paciente</h2>
-    </div>
+    <>
+      {" "}
+      <div className="text-center mt-5 container-titulo">
+        <h2>Agrega un paciente</h2>
+      </div>
       <div className="container-form">
-        <Form className="form" onSubmit={handleSubmit} >
+        <Form className="form" onSubmit={handleSubmit}>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Nombre y Apellido</Form.Label>
@@ -254,28 +255,27 @@ const Main = () => {
             </Form.Group>
           </Row>
           <Row>
-
-
             <div className="text-center mt-5">
-              {mostrar2 && <Button variant="primary" type="submit" onClick={handleChange}>
-                Agregar
-              </Button>}
+              {mostrar2 && (
+                <Button variant="primary" type="submit" onClick={handleChange}>
+                  Agregar
+                </Button>
+              )}
 
-              {mostrar && <Button variant="warning" type="button" onClick={handleActualizar}>
-                Actualizar
-              </Button>}
-
-
+              {mostrar && (
+                <Button
+                  variant="warning"
+                  type="button"
+                  onClick={handleActualizar}
+                >
+                  Actualizar
+                </Button>
+              )}
             </div>
-
-
-
           </Row>
         </Form>
       </div>
-
       <Sidebar />
-
       <Container className="tabla" fluid>
         <Table striped bordered hover>
           <thead>
@@ -355,16 +355,18 @@ const Main = () => {
                         />
                       </svg>
                     </button>
-                    <button className="editBtn" onClick={() => handleEditar(paciente.id_paciente)}
+                    <button
+                      className="editBtn"
+                      onClick={() => handleEditar(paciente.id_paciente)}
                     >
                       <svg height="1em" viewBox="0 0 512 512">
                         <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
                       </svg>
                     </button>
                     <Link className="link" to={`/hc/${paciente.id_paciente}`}>
-                    <button className="btn-ver">
-                      <i className="fa-solid fa-eye"></i>
-                    </button>
+                      <button className="btn-ver">
+                        <i className="fa-solid fa-eye"></i>
+                      </button>
                     </Link>
                   </div>
                 </td>
