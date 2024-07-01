092fe79a663eb4d9b8d4ce18fdc0d23e9b07b1bc
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-01 02:23:56 -0300

    "añado comentarios a los archivos de routes del back"

--- back/routes/hc.js
+++ back/routes/hc.js
@@ -1,4 +1,7 @@
+//importa express
 const express = require("express");
+
+//importa las funciones del archivo /controllers/hc
 const {
   todoHistoriasClinicas,
   agregarHistoriasClinicas,
@@ -7,12 +10,19 @@ const {
   verHistoriasClinicas,
 } = require("../controllers/hc");
 
+//crea la constante router que almacena la instancia del enrutador de express
 const router = express.Router();
 
+//Cada línea asigna una ruta y un método HTTP a una función controladora que maneja la solicitud
+// ruta:/hc. metodo HTTP:get. funcion controladora: todoHistorialClinicas.
+//Cuando se hace una solicitud GET a la ruta /hc, la función todoHistorialClinicas se ejecuta.
+//Esta función devuelve las historias clinicas
+//Y asi con todas las siguientes lineas
 router.get("/hc", todoHistoriasClinicas);
 router.post("/hc/agregar", agregarHistoriasClinicas);
 router.delete("/hc/eliminar/:id", borrarHistoriasClinicas);
 router.put("/hc/editar/:id", editarHistoriasClinicas);
 router.get("/hc/:id", verHistoriasClinicas);
 
+//exporta router
 module.exports = router;
--- back/routes/pacientes.js
+++ back/routes/pacientes.js
@@ -1,4 +1,7 @@
+//importa express
 const express = require("express");
+
+//importa las funciones del archivo /controllers/pacientes
 const {
   todoPacientes,
   agregarPacientes,
@@ -7,12 +10,19 @@ const {
   verPacientes,
 } = require("../controllers/pacientes");
 
+//crea la constante router que almacena la instancia del enrutador de express
 const router = express.Router();
 
+//Cada línea asigna una ruta y un método HTTP a una función controladora que maneja la solicitud
+// ruta:/pacientes. metodo HTTP:get. funcion controladora: todoPacientes.
+//Cuando se hace una solicitud GET a la ruta /pacientes, la función todoPacientes se ejecuta.
+//Esta función devuelve los pacientes
+//Y asi con todas las siguientes lineas
 router.get("/pacientes", todoPacientes);
 router.post("/pacientes/agregar", agregarPacientes);
 router.get("/pacientes/:id", verPacientes);
 router.delete("/pacientes/eliminar/:id", borrarPacientes);
 router.put("/pacientes/editar/:id", editarPacientes);
 
+//exporta router
 module.exports = router;
