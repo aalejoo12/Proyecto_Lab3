ca1f098b630adac948929613ee690fc2eadb97e9
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-02 14:59:15 -0300

    añado comentarios a los archivos de la carpeta controllers del back

--- back/controllers/camas.js
+++ back/controllers/camas.js
@@ -1,54 +1,82 @@
+//importa la conexion a la base de datos
 const { conection } = require("../config/db");
 
+//crea la funcion con los parametros request y response
 const todoCamas = (req, res) => {
+  //crea la query con el comando sql que sirve para ver las camas y su informacion con su respectiva sala
   const query = `select c.id_cama,c.fechaIngreso,c.fechaAlta,c.estado,s.tipoSala
 from Camas c 
 join Salas s
 on s.id_sala = c.id_sala
 order by c.id_cama`;
 
+  //ejecuta la consulta con la query y un callback que sirve para manejar los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la conexion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, muestra los resultados de la consulta en un json
     res.json(results);
   });
 };
 
+//crea la funciom con los argumentos request y response
 const agregarCamas = (req, res) => {
-  console.log(req);
+  //obtiene los valores de la nueva cama
   const { estado, fechaIngreso, fechaAlta, id_paciente, id_sala } = req.body;
-
+  //crea la query con el comando sql que añade una cama a la tabla camas en la base de datos. y le agrega los valores de la linea anterior
   const query = `INSERT INTO camas (estado, fechaIngreso, fechaAlta, id_paciente, id_sala) VALUES 
   ("${estado}","${fechaIngreso}","${fechaAlta}",${id_paciente},${id_sala})`;
 
+  //ejecuta la consulta con la query y un callback que sirve para manejar los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la conexion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados de haber añadido a la nueva cama
     res.send(results);
   });
 };
 
+//crea la funciom con los argumentos request y response
 const borrarCamas = (req, res) => {
+  //obtiene el id de la cama que hay que borrar
   const id = req.params.id;
+  //crea la query con el comando sql que sirve para borrar una cama de la tabla camas en la base de datos
   const query = `delete from camas where id_cama=${id}`;
+
+  //ejecuta la consulta con la query y un callback que sirve para manejar los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la conexion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados de haber borrado una cama
     res.send(results);
   });
 };
 
+//crea la funcion con los parametros request y response
 const editarCamas = (req, res) => {
+  //obtiene el id de la cama que se editara
   const id = req.params.id;
+  //obtiene los valores nuevos la cama que se editara
   const { estado, fechaIngreso, fechaAlta, id_paciente, id_sala } = req.body;
 
-
+  //crea la query con el comando sql que edita una cama de la base de datos, y le pasa los valores que se obtuvieron en la linea anterior
   const query = `UPDATE camas SET estado="${estado}",fechaIngreso="${fechaIngreso}",fechaAlta="${fechaAlta}", id_paciente="${id_paciente}",id_sala="${id_sala}" where  id_cama = ${id}`;
+
+  //ejecuta la consulta con la query y un callback que sirve para manejar los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados de la edicion
     res.send(results);
   });
 };
 
+//crea la funcion con los parametros request y response
 const verCamas = (req, res) => {
+  //obtiene el id de la cama que se quiere ver
   const id = req.params.id;
+
+  //crea la query con el comando sql que muestra la informacion de la cama, su sala y su respectivo paciente
   const query = `select c.id_cama,c.fechaIngreso,c.fechaAlta,c.estado,p.nombre,s.tipoSala
 from Camas c 
 join Salas s
@@ -58,12 +86,16 @@ on p.id_paciente = c.id_paciente
 where c.id_cama = ${id}
 order by c.id_cama;
 `;
+  //ejecuta la consulta con la query y un callback que sirve para manejar los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, muestra los resultados de la consulta
     res.send(results);
   });
 };
 
+//exporta las funciones
 module.exports = {
   todoCamas,
   agregarCamas,
--- back/controllers/salas.js
+++ back/controllers/salas.js
@@ -1,57 +1,90 @@
+//importa la conexion a la base de datos
 const { conection } = require("../config/db");
 
+//crea la funcion con los parametros request y response
 const todoSalas = (req, res) => {
+  //crea la query con el comando sql que sirve para mostrar la tabla salas
   const query = "select * from salas";
 
+  //ejecuta la consulta con la query y un callback que maneja los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, muestra los resultados de las salas en formato json
     res.json(results);
   });
 };
 
+//crea la funcion con los paramtros request y response
 const agregarSalas = (req, res) => {
-  console.log(req);
-  const {tipoSala, cantidadCamas } = req.body;
+  //obtiene los valores de la sala que se añadira
+  const { tipoSala, cantidadCamas } = req.body;
 
+  //crea la query con el comando sql que sirve para añadir una sala a la tabla salas y le da los valores que obtuvo en la linea anterior
   const query = `INSERT INTO salas (tipoSala, cantidadCamas) VALUES 
   ("${tipoSala}","${cantidadCamas}")`;
 
+  //ejecuta la consulta con la query y un callback que maneja los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error, detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados de haber añadido la sala
     res.send(results);
   });
 };
 
+//crea la funcion con los parametros request y response
 const borrarSalas = (req, res) => {
+  //obtiene el id de la sala que se borrara
   const id = req.params.id;
+  //crea la query con el comando sql que sirve para borrar una sala de la tabla salas
   const query = `delete from salas where id_sala=${id}`;
+
+  //ejecuta la consulta con la query y un callback que maneja los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error, detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados de haber eliminado una sala
     res.send(results);
   });
 };
 
+//crea la funcion con los parametros request y response
 const editarSalas = (req, res) => {
+  //obtiene el id de la sala que se editara
   const id = req.params.id;
-  const {tipoSala, cantidadCamas } = req.body;
+  //obtiene los valores nuevos que obtendra la cama
+  const { tipoSala, cantidadCamas } = req.body;
 
-
+  //crea la query con el comando sql que sirve para editar los valores de una cama, y le pasa los valores obtenidos en la linea anterior
   const query = `UPDATE salas SET tipoSala ="${tipoSala}",cantidadCamas="${cantidadCamas}" where  id_sala = ${id}`;
+
+  //ejecuta la consulta con la query y un callback que maneja los resultados de la consulta
   conection.query(query, (err, results) => {
+    //si hay un error detiene la ejecucion y señala que ha ocurrido un problema
     if (err) throw err;
+    //si no hay errores, envia los resultados con la cama editada
     res.send(results);
   });
 };
 
+//crea la funcion con los paramtetros request y response
 const verSalas = (req, res) => {
+  //obtiene el id de la sala que se quiere ver
   const id = req.params.id;
+  //crea la query con el comando sql que sirve para mirar la sala que tiene el id obtenido en la linea anterior
   const query = `select * from salas where id_sala=${id}`;
+
+  //ejecuta la consulta con la query y un callback que maneja los resultados de la consutla
   conection.query(query, (err, results) => {
+    //si hay un error, detiene la ejecucion y señala que ha ocurrid un problema
     if (err) throw err;
+    //si ni hay errores envia los resultados
     res.send(results);
   });
 };
 
+//exporta las funciones
 module.exports = {
   todoSalas,
   agregarSalas,
