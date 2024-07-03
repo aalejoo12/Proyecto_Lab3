819d4c227081ed42ee568b6d5478e6028c474e7a
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-03 05:44:08 -0300

    añado comentarios a main.jsx del front

--- HospitalTucuman/src/main.jsx
+++ HospitalTucuman/src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Importa las dependencias necesarias
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+// Renderiza la aplicación React dentro del elemento con el id 'root'
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Renderiza el componente App dentro del elemento seleccionado en el DOM
+  <App />
+);
