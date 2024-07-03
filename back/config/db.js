2e34af6039aab2e9481e378c595cca89e4369cad
bruno <bazanbruno199@gmail.com>
2024-07-03 10:34:08 -0300

    rama actualizada

--- back/config/db.js
+++ back/config/db.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const conection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "facu1234",
+  password: "1234",
   database: "HospitalTucuman2",
 });
 
