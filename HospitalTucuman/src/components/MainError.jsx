c6daf2ca2049d30395140262ffb38018349c90bd
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-07-03 05:31:05 -0300

    añado comentarios a los componentes del front

--- HospitalTucuman/src/components/MainError.jsx
+++ HospitalTucuman/src/components/MainError.jsx
@@ -1,16 +1,23 @@
+// Importa las librerías necesarias de React y otros módulos.
 import React from "react";
 import { Image, Row, Col } from "react-bootstrap";
 import Error from "../assets/error.png";
 import "../css/Error.css";
 import { Link } from "react-router-dom";
+
+// Define el componente funcional MainError.
 const MainError = () => {
   return (
     <>
+      {/* Contenedor principal */}
       <div className="container">
+        {/* Imagen de error */}
         <Image className="img" src={Error}></Image>
 
+        {/* Mensaje de error */}
         <h5>Pagina no encontrada</h5>
 
+        {/* Enlace para volver a la página principal */}
         <p style={{ marginLeft: "25px", marginTop: "10px" }}>
           <Link to={"/home"}>Volver a la pagina principal</Link>
         </p>
@@ -19,4 +26,5 @@ const MainError = () => {
   );
 };
 
+// Exporta el componente MainError para que pueda ser usado en otras partes de la aplicación.
 export default MainError;
--- HospitalTucuman/src/components/MainHome.jsx
+++ HospitalTucuman/src/components/MainHome.jsx
@@ -1,3 +1,4 @@
+// Importa las librerías y recursos necesarios.
 import React from "react";
 import { Row, Col, Image } from "react-bootstrap";
 import "../css/Home.css";
@@ -6,76 +7,91 @@ import Hospital from "../assets/hospital.png";
 import Habitacion from "../assets/habitacion.png";
 import Medicos from "../assets/medicos.png";
 import Medicine from "../assets/medicine.jpg";
+
+// Define el componente funcional MainHome.
 const MainHome = () => {
   return (
     <>
-    <div className="container-fluid">
-      <Row>
-        <Col className="izq letra">
-          <div style={{ marginTop: "5px" }}>
-            Ingresa en pacientes para añadir, actualizar o borrar la informacion
-            de algun paciente
-          </div>
-          <div style={{ marginTop: "10px" }}>
-            Ingresa en medicos para ver información de nuestros medicos
-          </div>
-          <div style={{ marginTop: "20px" }}>
-            Ingresa en turnos para ver información sobre los turnos
-          </div>
-          <div style={{ marginTop: "40px" }}>
-            Ingresa en historial para ver el historial
-          </div>
-          <div style={{ marginTop: "30px" }}>
-            Ingresa en estudios para ver la información de los estudios
-          </div>
-          <div style={{ marginTop: "20px" }}>
-            Ingresa en camas para ver la disponibilidad de las camas de nuestro
-            establecimiento
-          </div>
-        </Col>
-        <Col></Col>
-        <Col>
-          <h2 className="bienvenida" style={{ marginTop: "25px" }}>
-            Bienvenida/o al trabajo
-          </h2>
-          <p className="parrafo" style={{ marginTop: "30px" }}>
-            Te deseamos un buen dia. Estamos seguros de que tu talento,
-            dedicación y profesionalismo serán de gran ayuda para nosotros y
-            nuestros pacientes
-          </p>
-          <Row>
-            <Col>
-              <p className="typeWriter">
-                <Typewriter
-                  words={["Pasion", "Dedicación", "Esfuerzo", "Exito", "Salud"]}
-                  loop={3}
-                  typeSpeed={35}
-                  deleteSpeed={5}
-                  delaySpeed={1000}
-                />
-              </p>
-            </Col>
-            <Col>
-              <img className="foto" src={Medicine} alt="" />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col>
-          <Image className="img" src={Hospital}></Image>
-        </Col>
-        <Col>
-          <Image className="img" src={Habitacion}></Image>
-        </Col>
-        <Col>
-          <Image className="img" src={Medicos}></Image>
-        </Col>
-      </Row>
+      {/* Contenedor principal fluido */}
+      <div className="container-fluid">
+        <Row>
+          {/* Columna izquierda con texto informativo */}
+          <Col className="izq letra">
+            <div style={{ marginTop: "5px" }}>
+              Ingresa en pacientes para añadir, actualizar o borrar la
+              informacion de algun paciente
+            </div>
+            <div style={{ marginTop: "10px" }}>
+              Ingresa en medicos para ver información de nuestros medicos
+            </div>
+            <div style={{ marginTop: "20px" }}>
+              Ingresa en turnos para ver información sobre los turnos
+            </div>
+            <div style={{ marginTop: "40px" }}>
+              Ingresa en historial para ver el historial
+            </div>
+            <div style={{ marginTop: "30px" }}>
+              Ingresa en estudios para ver la información de los estudios
+            </div>
+            <div style={{ marginTop: "20px" }}>
+              Ingresa en camas para ver la disponibilidad de las camas de
+              nuestro establecimiento
+            </div>
+          </Col>
+          <Col></Col>
+          {/* Columna derecha con mensaje de bienvenida */}
+          <Col>
+            <h2 className="bienvenida" style={{ marginTop: "25px" }}>
+              Bienvenida/o al trabajo
+            </h2>
+            <p className="parrafo" style={{ marginTop: "30px" }}>
+              Te deseamos un buen dia. Estamos seguros de que tu talento,
+              dedicación y profesionalismo serán de gran ayuda para nosotros y
+              nuestros pacientes
+            </p>
+            <Row>
+              <Col>
+                {/* Componente Typewriter para mostrar palabras en animación de escritura */}
+                <p className="typeWriter">
+                  <Typewriter
+                    words={[
+                      "Pasion",
+                      "Dedicación",
+                      "Esfuerzo",
+                      "Exito",
+                      "Salud",
+                    ]}
+                    loop={3}
+                    typeSpeed={35}
+                    deleteSpeed={5}
+                    delaySpeed={1000}
+                  />
+                </p>
+              </Col>
+              {/* Imagen de medicina */}
+              <Col>
+                <img className="foto" src={Medicine} alt="" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          {/* Imágenes alineadas en columnas */}
+          <Col>
+            <Image className="img" src={Hospital}></Image>
+          </Col>
+          <Col>
+            <Image className="img" src={Habitacion}></Image>
+          </Col>
+          <Col>
+            <Image className="img" src={Medicos}></Image>
+          </Col>
+        </Row>
       </div>
     </>
   );
 };
 
+// Exporta el componente MainHome para que pueda ser usado en otras partes de la aplicación.
 export default MainHome;
